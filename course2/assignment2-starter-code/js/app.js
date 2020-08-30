72b237f46f854de247f88356ebab3cab14c91fe4
gvnikhil <gvnikhil21@gmail.com>
2020-08-31 01:04:26 +0530

    assignment2

---
+++ course2/assignment2-starter-code/js/app.js
@@ -0,0 +1,79 @@
+(function (){
+'use strict'
+
+angular.module('ShoppingListCheckOff',[])
+.controller('NewItemsToList',NewItemsToList)
+.controller('ToBuyController',ToBuyController)
+.controller('AlreadyBoughtController',AlreadyBoughtController)
+.service('ShoppingListCheckOffService',ShoppingListCheckOffService)
+
+NewItemsToList.$inject=['ShoppingListCheckOffService'];
+function NewItemsToList(ShoppingListCheckOffService){
+  var Item=this;
+  Item.newItemName="";
+  Item.newItemQuantity="";
+
+  Item.addItemToBuyList=function(){
+    ShoppingListCheckOffService.addToBuyItem(Item.newItemName,Item.newItemQuantity);
+  }
+}
+
+ToBuyController.$inject=['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+  var Buy=this;
+
+  Buy.items=ShoppingListCheckOffService.getToBuyItems();
+
+  Buy.addToBoughtAndRemoveToBuy=function (itemIndex){
+    ShoppingListCheckOffService.addToBoughtItem(itemIndex);
+  }
+}
+
+AlreadyBoughtController.$inject=['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+  var Bought=this;
+
+  Bought.items=ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService(){
+  var service=this;
+  var toBuyItems=[];
+
+  var toBoughtItems=[];
+
+  service.addToBuyItem=function (itemName,itemQuantity){
+    if(itemName===""||itemQuantity==="")
+    return;
+
+    var item={
+      name:itemName,
+      quantity:itemQuantity
+    };
+    toBuyItems.push(item);
+  }
+  service.addToBoughtItem=function (itemIndex){
+    var item={
+      name:toBuyItems[itemIndex].name,
+      quantity:toBuyItems[itemIndex].quantity
+    };
+    toBoughtItems.push(item);
+    service.removeToBuyItem(itemIndex);
+  }
+
+  service.removeToBuyItem=function (itemIndex){
+    toBuyItems.splice(itemIndex,1);
+  }
+
+  service.getToBuyItems=function (){
+    return toBuyItems;
+  }
+  service.getBoughtItems=function (){
+    return toBoughtItems;
+  }
+
+
+}
+
+
+})();
